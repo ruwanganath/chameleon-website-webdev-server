5bdeb178c626ae60cadac3f988dd6c83a6aa9151
ruwanganath <ruwanganath@hotmail.com>
2022-04-28 13:20:55 +1000

    Initial file structure for server

---
+++ README.md
@@ -0,0 +1,14 @@
+# Chameleon company website
+MongoDB, Express, Vue.js, Node.js Boilerplate
+
+
+
+## Setup
+-------------------
+cd client  
+npm run dev
+
+cd server  
+npm start
+
+Open http://localhost8080 in browser
---
+++ src/app.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const morgan = require('morgan')
+const app = express()
+app.use(morgan('combined'))
+app.use(bodyParser.json())
+app.use(cors())
+
+const mongoose = require('mongoose')
+mongoose.connect('mongodb+srv://chameleon_admin:GcWYlcK0xADZ8gth@cluster0.i6hsu.mongodb.net/chameleondb?authSource=admin&replicaSet=atlas-13myik-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true',{useNewUrlParser: true,useUnifiedTopology: true, useCreateIndex: true})
+const db = mongoose.connection
+db.on('error', console.error.bind(console, 'connection error'))
+db.once('open', function (callback) {
+  console.log('mongoDb Connection Succeeded')
+})
+
+require('./routes')(app)
+
+app.listen(process.env.PORT || 8081)
